8e38d0968ba0ccbe1110d5ae45b1516f57035ca4
hiexy <hiexy@ubuntu>
2022-01-12 23:26:41 -0800

    Fix functions for Interoperability with Testing modules

--- packages/auth_mech.py
+++ packages/auth_mech.py
@@ -15,16 +15,15 @@ def register(users):
     user = users.find(myquery)
     for i in user:
         if i['username'] == d['username']:
-            print('Account name already exists. Please choose a unique name!')
             flag = True
     if flag:
-        return
+        return 'Account name already exists. Please choose a unique name!'
 
     password1 = getpass('Enter password: ')
     password2 = getpass('Confirm password: ')
     if password1 != password2:
-        print('Passwords do not match')
-        return
+        return 'Passwords do not match'
+        
     
     salt = os.urandom(32)
     key = hashlib.pbkdf2_hmac('sha256', password1.encode('utf-8'), salt, 100000)
@@ -33,7 +32,7 @@ def register(users):
     d['hash'] = key.hex()
     
     users.insert_one(d)
-    print('Account successfully created.')
+    return 'Account successfully created.'
 
 def login(users):
     d = dict()
@@ -46,13 +45,12 @@ def login(users):
     user = users.find(myquery)
     x = list(user)
     if len(x) == 0:
-            print('Incorrect Username/Password.')
-            return
+        return 'Incorrect Username/Password.'
 
     for i in x:
         if i['username'] == d['username']:
             key = hashlib.pbkdf2_hmac('sha256', d['password'].encode('utf-8'), bytes.fromhex(i['salt']), 100000)
             if key.hex() == i['hash']:
-                print('Logged In')
+                return 'Logged In'
             else:
-                print('Incorrect Username/Password.')
+                return 'Incorrect Username/Password.'
